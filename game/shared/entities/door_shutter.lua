6c3ffd91e6c9ccf0552859bfcdf375451f74f6bf
Tyler Duffus <tyled94@hotmail.com>
2020-10-03 13:57:08 +1000

    Added door_shutter start open property

--- game/shared/entities/door_shutter.lua
+++ game/shared/entities/door_shutter.lua
@@ -6,9 +6,25 @@ entities.linkToClassname(door_shutter, "door_shutter")
 function door_shutter:door_shutter()
 	door.door(self)
 
+	self.startOpen = false
+
 	if (_CLIENT) then
 		local spr = sprite("images.entities.door_shutter")
 		spr:setFilter("nearest", "nearest")
 		self:setSprite(spr)
+	end
+end
+
+function door_shutter:setProperties(properties)
+	door.setProperties(self, properties)
+
+	self.startOpen = toboolean(properties["startOpen"])
+end
+
+function door_shutter:spawn()
+	door.spawn(self)
+
+	if (self.startOpen) then
+		self:setAnimation("idleopen")
 	end
 end
