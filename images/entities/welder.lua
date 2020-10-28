13172c6806a904c8a004e384a03d0c1da6c9c1c1
Tyler Duffus <tyled94@hotmail.com>
2020-10-28 13:46:10 +1000

    Added missing welders image script

---
+++ images/entities/welder.lua
@@ -0,0 +1,40 @@
+return {
+	image = "entities/welder.png",
+	width = 32,
+	height = 32,
+	frametime = 0.1,
+	animations = {
+		welder = { 1 },
+		welder_on = { 2, 3 },
+		welder_100 = { 4 },
+		welder_75 = { 5 },
+		welder_50 = { 6 },
+		welder_25 = { 7 },
+		welder_0 = { 8, 9 },
+
+		ind = { 10 },
+		ind_on = { 11, 12 },
+		ind_100 = { 13 },
+		ind_75 = { 14 },
+		ind_50 = { 15 },
+		ind_25 = { 16 },
+		ind_0 = { 17 },
+
+		mini = { 19 },
+		mini_on = { 20, 21 },
+
+		upind = { 28 },
+		upind_on = { 29, 30 },
+		upind_100 = { 31 },
+		upind_75 = { 32 },
+		upind_50 = { 33 },
+		upind_25 = { 34 },
+		upind_0 = { 35 },
+
+		ex = { 37, 40 },
+		ex_on = { 41, 42 },
+
+		brass = { 46 },
+		brass_on = { 47, 48 }
+	}
+}
