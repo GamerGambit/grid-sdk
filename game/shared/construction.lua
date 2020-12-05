cf302eced1b4955766c9e9ab7a439df5a09c6e44
Tyler Duffus <tyled94@hotmail.com>
2020-12-05 12:47:59 +1000

    Added construction recipe grouping

--- game/shared/construction.lua
+++ game/shared/construction.lua
@@ -13,22 +13,21 @@
 
 	_lookup = {
 		"girder" = {
-			"tool.wrench" = {
-					onActivate = function(ent, act) ent.secured = not ent.secured end
+			"tool_wrench" = {
+					onActivate = function(ent, act, item) ent.secured = not ent.secured end
 				},
-			"item.rod" = {
-					predicate = function(ent, act) return true end, -- rods are always welcome
-					onActivate = function(ent, act) ent.reinforced = true end
+			"item_rod" = {
+					predicate = function(ent, act, item) return true end, -- rods are always welcome
+					onActivate = function(ent, act, item) ent.reinforced = true end
 				},
-			"item.plasteel" = {
-					predicate = function(ent, act) return ent.secured and ent.reinforced end,
-					onActivate = function(ent, act)
+			"item_plasteel" = {
+					predicate = function(ent, act, item) return ent.secured and ent.reinforced end,
+					onActivate = function(ent, act, item)
 							local rw = ents.create("reinforced_wall")
 							rw:setPosition(ent:getPosition())
 							rw:spawn()
 							ent:remove()
-						end,
-					onFailed = function(ent, act) print("Girder must be reinforced, and secured to the floor.") end
+						end
 				}
 		}
 	}
@@ -36,34 +35,31 @@
 --[[
 	RECIPE
 
-	`predicate`  (ent, act) = callback to check if the recipe is available given the entity's current state. If `predicate` is not a function, it is assumed to return true. (probably in the callback)
-	`onActivate` (ent, act) = callback when the predicate succeeds. Any item consumption should be done here.
-	`onFailed`   (ent, act) = callback when the predicate fails
+	`predicate`  (ent, act, item) = callback to check if the recipe is available given the entity's current state. If `predicate` is not a function, it is assumed to return true. (probably in the callback)
+	`onActivate` (ent, act, item) = callback when the predicate succeeds. Any item consumption should be done here
 ]]
 
 require "engine.shared.entities"
 
 local assert        = assert
 local entities      = entities
-local error         = error
+local ipairs        = ipairs
 local pairs         = pairs
-local print         = print
 local strFormat     = string.format
 local tblShallowCopy = table.shallowcopy
 local tblMerge      = table.merge
-local tblHasValue   = table.hasvalue
+local tostring      = tostring
 local type          = type
 local typeof        = typeof
 
 local recipeMT = {
-	predicate  = function(ent, act) return true end,
-	onActivate = function(ent, act) end,
-	onFailed   = function(ent, act) end,
+	predicate  = function(ent, act, item) return true end,
+	onActivate = function(ent, act, item) end
 }
 
-local _lookup = {}
-
 module "construction"
+
+_lookup = _lookup or {}
 
 function check(ent, activator)
 	local recipes = _lookup[type(ent)]
@@ -83,17 +79,20 @@ function check(ent, activator)
 		if (not found) then return false end
 	end
 
-	local itemType = type(activator:getActiveHand():getItem())
-	local recipe = recipes[itemType]
+	-- TODO add more checks here for things like mech suits/vehicles
+	-- what if the activator does not have arms? like a robot?
+	local item = activator:getActiveHand():getItem()
+	local itemType = type(item)
+	local itemRecipes = recipes[itemType]
 
 	 -- if no recipe is registered for the exact item type, try checking base classes
-	if (type(recipe) ~= "table") then
+	if (type(itemRecipes) ~= "table") then
 		local found = false
 
 		for k,v in pairs(recipes) do
-			if (typeof(entities.getClassMap()[itemType], k)) then
+			if (typeof(itemType, k)) then
 				found = true
-				recipe = v
+				itemRecipes = v
 				break
 			end
 		end
@@ -101,36 +100,47 @@ function check(ent, activator)
 		if (not found) then return false end
 	end
 
-	if (not recipe.predicate(ent, activator)) then
-		recipe.onFailed(ent, activator)
-		return true
+	for _, recipe in ipairs(itemRecipes) do
+		if (recipe.predicate(ent, activator, item)) then
+			--[[
+			-- DEPRECATED, moved to `onActivate`
+			-- TODO maybe revisit this later. One idea I had was to make `onActivate` return a table of items to spawn,
+			--	however one problem was dealing with things like metal sheets which have a quantity but are represented by a single entity.
+			--	One solution I had was to make `onActivate` return a mixed table of strings/pairs/tuples like `wall = { "girder" , { "metal_sheet", 10 } }`
+			--	"girder" would be treated as an object to spawn in. "metal_sheet" would be treated the same, except the "10" would be passed to a "setup" function.
+
+			local res = nil
+			if (type(recipe.result) == "string") then
+				assert(tblHasValue(entsGetAll(), recipe.result), strFormat("Result entity %q does not exist for recipe %s|%s", recipe.result, type(ent), itemType))
+
+				-- TODO how does `ent` rotation translate to `res` rotation?
+				local pos = ent:getPosition()
+				ent:remove()
+				res = entity.create(recipe.result)
+				res:setPosition(pos)
+				res:spawn()
+			end
+
+			if (type(recipe.onActivate) == "function") then recipe.onActivate(ent, activator, res) end
+			]]
+
+			recipe.onActivate(ent, activator, item)
+		end
 	end
-
-	--[[
-	-- DEPRECATED, moved to `onActivate`
-	-- TODO maybe revisit this later. One idea I had was to make `onActivate` return a table of items to spawn,
-	--	however one problem was dealing with things like metal sheets which have a quantity but are represented by a single entity.
-	--	One solution I had was to make `onActivate` return a mixed table of strings/pairs/tuples like `wall = { "girder" , { "metal_sheet", 10 } }`
-	--	"girder" would be treated as an object to spawn in. "metal_sheet" would be treated the same, except the "10" would be passed to a "setup" function.
-
-	local res = nil
-	if (type(recipe.result) == "string") then
-		assert(tblHasValue(entsGetAll(), recipe.result), strFormat("Result entity %q does not exist for recipe %s|%s", recipe.result, type(ent), itemType))
-
-		-- TODO how does `ent` rotation translate to `res` rotation?
-		local pos = ent:getPosition()
-		ent:remove()
-		res = entity.create(recipe.result)
-		res:setPosition(pos)
-		res:spawn()
-	end
-
-	if (type(recipe.onActivate) == "function") then recipe.onActivate(ent, activator, res) end
-	]]
-
-	recipe.onActivate(ent, activator)
 
 	return true
+end
+
+local function registerItem(entType, itemType, recipeTbl)
+	local allEnts = entities.getAll()
+	entities.require(itemType)
+	--assert(tblHasValue(allEnts, itemType), strFormat("Invalid item type %q for recipe %q", itemType, entType))
+
+	local newRecipe = tblShallowCopy(recipeMT)
+	tblMerge(newRecipe, recipeTbl)
+
+	_lookup[entType][itemType] = _lookup[entType][itemType] or {}
+	table.insert(_lookup[entType][itemType], newRecipe)
 end
 
 function register(entType, recipesTbl)
@@ -139,20 +149,18 @@ function register(entType, recipesTbl)
 	--assert(not tblHasValue(allEnts, entType), strFormat("Invalid entity type %q for recipe", entType))
 	assert(type(recipesTbl) == "table", strFormat("\"recipeTbl\" should be a table for recipe %q", entType))
 
-	if (type(_lookup[entType]) ~= "table") then _lookup[entType] = {} end
+	_lookup[entType] = _lookup[entType] or {}
 
 	for itemType, recipeTbl in pairs(recipesTbl) do
-		entities.require(itemType)
-		assert(tblHasValue(allEnts, itemType), strFormat("Invalid item type %q for recipe %q", itemType, entType))
-
-		if (type(_lookup[entType][itemType]) == "table") then
-			-- TODO this should be changed to use a more formal warning system
-			print(strFormat("WARNING: A recipe already exists for %q / %q, but overwriting.", entType, itemType))
+		if (type(itemType) == "string") then
+			registerItem(entType, itemType, recipeTbl)
+		elseif (type(itemType) == "number") then -- if the itemType is a number, we expect `recipeTbl` to be `N = { items = { ... }, recipe = { ... } }`
+			assert(type(recipeTbl.items) == "table") -- TODO add message
+			assert(type(recipeTbl.recipe) == "table", strFormat("No recipe table found in %s/%s", entType, tostring(itemType))) -- TODO add message
+			for _, itemType in ipairs(recipeTbl.items) do
+				registerItem(entType, itemType, recipeTbl.recipe)
+			end
 		end
-
-		local newRecipe = tblShallowCopy(recipeMT)
-		tblMerge(newRecipe, recipeTbl)
-		_lookup[entType][itemType] = newRecipe
 	end
 end
 
